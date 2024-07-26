77f3c34ea003c054cd96af9bfb5839fca82a2ce3
Arturocs160 <kronculd@outlook.com>
2024-07-25 20:30:51 -0600

    Correción de errores de las funcionalidades de agregar,editar productos y servicios(Imagenes)

--- app.js
+++ app.js
@@ -366,6 +366,90 @@ app.delete('/servicios/:id_Servicio', (req, res) => {
     });
   });
   
+//Agendar cita
+
+app.post('/agendarCita', async(req, res) => {
+    const {nombre_Cliente, apellido1, apellido2, telefono, correo, fecha, hora, id_Servicio} = req.body;
+
+    connection.query('INSERT INTO citas SET  ?',
+        {nombre_Cliente:nombre_Cliente, apellido1:apellido1, apellido2:apellido2, telefono:telefono,correo:correo, fecha:fecha,
+          hora:hora, id_Servicio:id_Servicio},
+      async (error, result) => {
+        if(error){
+            console.log("Error " + error);
+        }
+        else{
+          res.json({ id: result.insertId });
+        }
+   });
+});
+
+
+// Obtener horas de citas disponibles
+app.get('/api/citas/:date', (req, res) => {
+  const date = req.params.date;
+  const query = 'SELECT TIME_FORMAT(hora, "%H:%i") AS hora FROM citas WHERE fecha = ?';
+
+  connection.query(query, [date], (err, results) => {
+      if (err) {
+          return res.status(500).send(err);
+      }
+      const reservedTimes = results.map(cita => cita.hora);
+      res.json(reservedTimes);
+  });
+});
+
+// Obtener los datos de las citas
+app.get('/citas/datos/:num_Cita', (req, res) => {
+  const num_Cita = req.params.num_Cita;
+  const query = 'SELECT * FROM citas where num_Cita = ?';
+  db.query(query, [num_Cita], (err, results) => {
+    if (err) throw err;
+    res.json(results);
+  });
+});
+
+
+// Ruta para obtener la cita de un usuario mediante correo y número de cita
+app.get('/consultarInformacionCitas', async(req, res) => {
+  const { num_Cita, correo } = req.query;
+
+  if (!num_Cita || !correo) {
+    return res.status(400).json({ error: 'Email y número de cita son requeridos' });
+  }
+  
+  const query = 'SELECT * FROM citas WHERE num_Cita = ? AND correo = ?';
+  connection.query(query, [num_Cita, correo], (err, results) => {
+      if (err) {
+          return res.status(500).json({ error: err.message });
+      }
+      if (results.length > 0) {
+          res.json(results[0]);
+      } else {
+          res.status(404).json({ message: 'Cita no encontrada' });
+      }
+  });
+});
+
+// Ruta para cancelar la cita
+app.delete('/api/cancelar-cita/:num_Cita', (req, res) => {
+  const num_Cita = req.params.num_Cita;
+
+  const query = 'DELETE FROM citas WHERE num_Cita = ?';
+
+  connection.query(query, [num_Cita], (error, results) => {
+      if (error) {
+          console.error('Error al cancelar la cita:', error);
+          res.status(500).send('Error al cancelar la cita.');
+      } else {
+          if (results.affectedRows > 0) {
+              res.status(200).send('Cita cancelada con éxito.');
+          } else {
+              res.status(404).send('Cita no encontrada.');
+          }
+      }
+  });
+});
 
 app.listen(3000, (req,res)=>{
     console.log("SERVER RUNNING IN http://localhost/3000");
